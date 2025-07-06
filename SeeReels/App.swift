e3d5bf072d3a9da12a258ea0ea278bf870cee17b
Ryan F <forsyth.r@gmail.com>
2025-07-06 10:20:15 +0100

    🚀 Project: Initial commit
    - Empty Xcode 16.4 (16F6) project with iOS app target
    - Set minimum iOS to 18.0
    - Add empty Docc documentation folder
    - Add empty String Catalog, French to project localizations
    - Add gitignore for common/machine-specific Xcode files that shouldn't be committed

---
+++ SeeReels/App.swift
@@ -0,0 +1,17 @@
+//
+//  App.swift
+//  SeeReels
+//
+//  Created by Ryan on 2025-07-06.
+//
+
+import SwiftUI
+
+@main
+struct SeeReelsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            StoryListView()
+        }
+    }
+}
